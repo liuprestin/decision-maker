ddd1086181266bea31c0925150039d04dd85e1f3
Prestin <liuprestin@users.noreply.github.com>
2023-07-24 09:10:40 -0700

    Add the new routes survey, insight and response, add comments for loading other dependencies into the project

--- app.js
+++ app.js
@@ -5,22 +5,34 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var surveyRouter = require('./routes/survey');
+var insightRouter = require('./routes/insight');
+var responseRouter = require('./routes/response');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+// --> may need to add Bootstrap
+// --> may need to add POSTCSS or SASS here 
 
+
+// middlewere
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// --> add nodemon
+// --> add cors 
+
+// setup our routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/survey', surveyRouter);
+app.use('/insight', surveyRouter);
+app.use('/response', surveyRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
