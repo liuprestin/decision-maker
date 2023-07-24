ed3503ec650878bd548be273d2dc2ba9deeb68d3
Prestin <liuprestin@users.noreply.github.com>
2023-07-24 09:23:27 -0700

    Switch the template engine to EJS instead

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs'); 
 // --> may need to add Bootstrap
 // --> may need to add POSTCSS or SASS here 
 
