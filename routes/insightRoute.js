c46d7d3e1a6081f64f44caa9a22ec655af8f7f23
Prestin Liu <liuprestin@proton.me>
2023-07-29 02:47:15 -0700

    add comment

--- routes/insightRoute.js
+++ routes/insightRoute.js
@@ -2,8 +2,20 @@ const express = require("express");
 const database = require("../db/database");
 const router = express.Router();
 
-router.get('/insight/:id', function(req, res, next) {
-    res.send('respond with a resource' + req.params.id);
+// Route originally to be posting the borden count
+// of ranked answers from specified survey
+
+router.get('/insight/:adminUrl', (req, res) => {
+  const adminUrl = req.params.adminUrl;
+  database
+    .getSurveyDataByAdminUrl(adminUrl)
+    .then((data) => {
+      if (!data) {
+        return res.send({ error: "No survey data found for this adminUrl." });
+      }
+      res.send(data);
+    })
+    .catch((err) => res.send(err));
   });
 
 module.exports = router;
